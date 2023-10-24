38f753946356daa8235928d011b761cec0d1f4a6
Artur Z <arturziianbaev@gmail.com>
2023-10-23 17:44:40 -0700

    feature to shoot bullets

---
+++ bullets.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+
+class Bullets(Turtle):
+    def __init__(self,x_start):
+        super().__init__()
+        self.color('yellow')
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_len=0.25, stretch_wid=0.5)
+        self.goto(x=x_start,y=-230)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle as tr
 from gamer import  Gamer
 import time
+from bullets import Bullets
 screen=tr.Screen()
 screen.title('Space Invaders Game')
 screen.setup(width=720,height=576)
@@ -8,17 +9,26 @@ screen.bgcolor('black')
 screen.tracer(0)
 
 gamer=Gamer()
-
+bullets=[]
+def create_bullet():
+    bullets.append(Bullets(gamer.xcor()))
 screen.listen()
 screen.onkey(gamer.go_left, 'a')
 screen.onkey(gamer.go_right,'d')
 screen.onkey(gamer.go_right,'Right')
 screen.onkey(gamer.go_left,'Left')
-
+screen.onkey(create_bullet,'space')
 game_is_on=True
+
 while game_is_on:
     time.sleep(0.05)
     screen.update()
+
+    print(bullets)
+
+    for each in bullets:
+        each.goto(x=each.xcor(),y=each.ycor() + 10)
+
 
 
 
