125200307022f0f99c09d5f4be6efd55a96c316f
Artur Z <arturziianbaev@gmail.com>
2023-10-23 17:36:09 -0700

    screen update

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import turtle as tr
-from gamer import Gamer
-
+from gamer import  Gamer
+import time
 screen=tr.Screen()
 screen.title('Space Invaders Game')
 screen.setup(width=720,height=576)
 screen.bgcolor('black')
+screen.tracer(0)
 
 gamer=Gamer()
 
 screen.listen()
-
 screen.onkey(gamer.go_left, 'a')
 screen.onkey(gamer.go_right,'d')
-
-
 screen.onkey(gamer.go_right,'Right')
 screen.onkey(gamer.go_left,'Left')
+
+game_is_on=True
+while game_is_on:
+    time.sleep(0.05)
+    screen.update()
+
+
 
 
 
